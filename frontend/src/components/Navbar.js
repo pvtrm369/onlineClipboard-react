809455725efa97d66e87c9ca7c8b9abdc650dfdb
Pavitra Verma <138149242+pvtrm369@users.noreply.github.com>
2025-02-23 14:07:36 +0530

    ui imp

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
         <div>
 
             <nav className="navbar navbar-light bg-primary justify-content-center">
-                <span className="navbar-brand mb-0 h1 text-light">Clipboard</span>
+                <span className="navbar-brand mb-0 h1 text-light">Textport</span>
             </nav>
         </div>
     )
